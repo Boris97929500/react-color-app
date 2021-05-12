464e1a91f4bccc05d079873279cf7aacdff68b6d
Boris <boris@email.com>
2021-05-12 15:54:46 +1000

    third commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
+import { Route, Switch } from 'react-router-dom';
 import Palette from "./Palette";
+import PaletteList from './PaletteList';
 import seedColors from "./seedColors";
+import {generatePalette} from './colorHelpers';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.findPalette = this.findPalette.bind(this);
+  }
+
+  findPalette(id){
+    return seedColors.find( function(palette){
+      return palette.id===id
+    }  );
+  }
   render() {
     return (
-      <div>
-        <Palette {...seedColors[5]} />
-      </div>
+      <Switch>
+        <Route exact path='/' render={ ()=><PaletteList palettes={seedColors}/> } />
+        <Route exact path='/palette/:id' render={
+          routeProps=><Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />}/>
+        <Route  path='*' exact render={ ()=><h1>404 PAGE NOT FOUND</h1> } />
+      </Switch>
+
+
     );
   }
 }
--- src/Palette.js
+++ src/Palette.js
@@ -1,20 +1,42 @@
 import React, { Component } from "react";
 import ColorBox from './ColorBox';
+import Navbar from './Navbar';
+// import 'rc-slider/assets/index.css';
 import './Palette.css';
+// import Slider from 'rc-slider';
+
 
 class Palette extends Component {
+  constructor(props){
+    super(props);
+    this.state= {level:500, format:'hex'};
+    this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
+  }
+  changeLevel(newLevel){
+    this.setState({level:newLevel})
+  }
+  changeFormat(val){
+    this.setState({format:val});
+  }
+
   render() {
-    const colorBoxes = this.props.colors.map(color=>(
-      <ColorBox background={color.color} name={color.name} />
+    const { level, format } = this.state;
+    const {colors, paletteName} = this.props.palette;
+    const colorBoxes = colors[level].map(color=>(
+      <ColorBox background={color[format]} name={color.name} key={color.id} />
     ))
 
     return (
       <div className='Palette'>
-        {/* Navbar goes here */}
+        <Navbar level={this.state.level} changeLevel={this.changeLevel} handleChange={this.changeFormat} />
         <div className='Palette-colors'>
           {colorBoxes}
         </div>
-        {/* footer eventually */}
+        <footer className='Palette-footer'>
+          {paletteName}
+          {/* <span className='emoji' >{emoji}</span> */}
+        </footer>
       </div>
     );
   }
